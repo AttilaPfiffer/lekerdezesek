825a0070fba1d8e6b51767fc537c99919ebddc1b
AttilaPfiffer <pifuati@gmail.com>
2024-11-05 15:55:00 +0100

    lekerdezesek

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\LendingController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//bárki által elérhető
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
+
+//összes kérés
+Route::apiResource('/users', UserController::class);
+Route::patch('update-password/{id}', [UserController::class, "updatePassword"]);
+
+//autentikált útvonal
+Route::middleware(['auth:sanctum'])
+    ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::get('lendings-copies', [LendingController::class, "lendingsWithCopies"]);
+        Route::get('user-lendings', [UserController::class, "userLendings"]);
+        Route::get('specific-date', [LendingController::class, "dateSpecific"]);
+        // Kijelentkezés útvonal
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    });
+
+//admin
+Route::middleware(['auth:sanctum',Admin::class])
+->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index']);
+    Route::get('specific-copy/{copy_id}', [LendingController::class, "copySpecific"]);
+});
+
+
+Route::get('books-copies', [BookController::class, 'booksWithCopies']);
+
+
+
